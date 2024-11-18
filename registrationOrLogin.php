fab5c9e73db4c4c94a4536aaa3620f648c0d8bcc
za11ka <borodulenkoila@gmail.com>
2024-11-18 08:57:39 +0300

    mysql

--- registrationOrLogin.php
+++ registrationOrLogin.php
@@ -32,16 +32,12 @@
     <div>
         <img class="profile1" src="image/bigprofile.png">
         <br>
-        <form action="">
-            <label class ="label_mail"for="mail">Введите телефон</label><br>
-            <input type="tel" name="mail" class ="pochta">
-            <br>
-            <br>    
-            <label class = "label_sms" for="sms">Введите пароль</label> <br>
-            <input type="password" name="sms" class="sms"><br>
-
-            <button class="neon-button">Отправить</button>
-
+        <form action="register.php" method = "post">
+            <input type="text" name="login" class ="pochta">
+            <input type="text" name="pass" class ="pochta">
+            <input type="text" name="repeatpass" class ="pochta">   
+            <input type="text" name="email" class="sms"><br>
+            <button type = "submit" class="neon-button">Регистрация</button>
         </form><br>
     </div>
 </body>
